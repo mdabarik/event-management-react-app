7d27b6910d6958a897780a71d171b99dc591c85f
mdabarik <mdabarik19@gmail.com>
2023-10-07 00:48:06 +0600

    aos effect added

--- src/components/BgImage/BgImage.jsx
+++ src/components/BgImage/BgImage.jsx
@@ -1,8 +1,14 @@
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 const BgImage = ({ image }) => {
+    useEffect(() => {
+        AOS.init();
+    }, [])
+
     return (
-        <div className="border-red-600 w-[100vw] h-[85%] absolute top-0 -z-10">
+        <div data-aos="zoom-in-up" className="border-red-600 w-[100vw] h-[85%] absolute top-0 -z-10">
             <img className="w-[100%] h-[100%] object-cover" src={image} alt="logo" />
             <div className="w-[100vw] h-[100%] border-2 border-orange-500 bg-[#00000070] absolute top-0 left-0 z-10"></div>
         </div>
